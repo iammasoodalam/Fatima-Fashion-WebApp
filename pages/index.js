d63ce48e776b4df146422ea82627d5d9d546f65e
Mohd Masood Alam <130masoodalam@gmail.com>
2023-02-15 23:18:17 +0530

    removed all codes for different sections to their native pages

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,17 @@ import styles from "../styles/Home.module.scss";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import Link from "next/link";
+import { useState } from "react";
+import Tasks from "../components/tasks";
+import Profile from "../components/Profile";
 
 export default function Home() {
   const { user, error, isLoading } = useUser();
+  const [activeNav, setActiveNav] = useState(1);
+
+  function changeNav(e) {
+    setActiveNav(e.currentTarget.tabIndex);
+  }
 
   return (
     <>
@@ -20,21 +28,17 @@ export default function Home() {
             boutique
           </p>
           <Link href="/api/auth/login" className={styles.login}>
-            <div >Get Started</div>
+            <div>Get Started</div>
           </Link>
         </div>
       )}
 
       {user && (
-        <div>
+        <div className={styles.main}>
           <Header />
-          <NavBar />
-          <img src={user.picture} alt={user.name} />
-          <h3>{user.name}</h3>
-          <p>{user.email}</p>
-          <div className={styles.login}>
-            <Link href="/api/auth/logout">Logout</Link>
-          </div>
+          {activeNav == 4 && <Profile />}
+          {activeNav === 2 && <Tasks />}
+          <NavBar activeNav={activeNav} changeNav={changeNav} />
         </div>
       )}
     </>
